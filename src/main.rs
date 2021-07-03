b8aee65f4ca22b5e4c24b57afe3c49813e106472
Zachary Kohnen <me@dusterthefirst.com>
2021-07-02 21:32:05 -0400

    Reading and parsing all the data from netlists

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use anyhow::Context;
+use lexpr::Value;
 use regex::Regex;
-use std::{fs, path::PathBuf};
+use sexpr::Text;
+use std::{collections::HashMap, fs, hash::Hash, path::PathBuf};
 
 use argh::FromArgs;
 
-use crate::netlist::Netlist;
+use crate::sexpr::IntoText;
 
-mod netlist;
 mod sexpr;
 
 /// Synchronize your pinouts between firmware and electrical designs.
@@ -20,7 +21,7 @@ fn main() -> anyhow::Result<()> {
     let args: Arguments = argh::from_env();
     dbg!(&args);
 
-    let netlist_sexpr = {
+    let sexpr = {
         let netlist_file =
             fs::read_to_string(args.netlist).context("Failed to open the provided netlist file")?;
 
@@ -30,9 +31,146 @@ fn main() -> anyhow::Result<()> {
             .context("Failed to parse the given netlist file")?
     };
 
-    let netlist = Netlist::new(&netlist_sexpr);
+    let (title, company, rev) = {
+        let title_block = &sexpr["design"]["sheet"]["title_block"];
 
-    dbg!(&netlist);
+        (
+            title_block["title"].text_join(),
+            title_block["company"].text_join(),
+            title_block["rev"].text_join(),
+        )
+    };
+
+    let nets = sexpr["nets"]
+        .list_iter()
+        .unwrap()
+        .map(|net| {
+            let name = net["name"].text_join();
+
+            net.list_iter().unwrap().skip(2).map(move |node| {
+                let reference = node["ref"].text_join();
+                let pin = node["pin"].text_join();
+
+                ((reference, pin), name.clone())
+            })
+        })
+        .flatten()
+        .collect::<HashMap<_, _>>();
+
+    dbg!(nets);
+
+    let pins = sexpr["libparts"]
+        .list_iter()
+        .unwrap()
+        .map(|v| {
+            v["pins"]
+                .list_iter()
+                .map(|pins| (LibraryPart::new(&v), pins.map(Pin::new).collect::<Vec<_>>()))
+        })
+        .filter_map(|a| a)
+        .collect::<HashMap<_, _>>();
+
+    // dbg!(pins);
+
+    let components = sexpr["components"]
+        .list_iter()
+        .unwrap()
+        .map(|v| {
+            let component = Component::new(v);
+            let pins = pins.get(&component.libpart);
+
+            (v["ref"].text_join(), (component, pins))
+        })
+        .collect::<HashMap<_, _>>();
+
+    // dbg!(components);
 
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub struct Component<'t> {
+    libpart: LibraryPart<'t>,
+    value: Text<'t>,
+    description: Text<'t>,
+    footprint: Text<'t>,
+    datasheet: Text<'t>,
+}
+
+impl<'t> Component<'t> {
+    pub fn new(value: &'t Value) -> Self {
+        Self {
+            libpart: LibraryPart::new(&value["libsource"]),
+            description: value["libsource"]["description"].text_join(),
+            value: value["value"].text_join(),
+            footprint: value["footprint"].text_join(),
+            datasheet: value["datasheet"].text_join(),
+        }
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, PartialOrd, Ord, Clone)]
+pub struct LibraryPart<'t> {
+    lib: Text<'t>,
+    part: Text<'t>,
+}
+
+impl<'t> LibraryPart<'t> {
+    pub fn new(value: &'t Value) -> Self {
+        Self {
+            lib: value["lib"].text_join(),
+            part: value["part"].text_join(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Pin<'t> {
+    num: Text<'t>,
+    name: Text<'t>,
+    ty: PinType,
+    // net: Net
+}
+
+impl<'t> Pin<'t> {
+    pub fn new(value: &'t Value) -> Self {
+        Self {
+            num: value["num"].text_join(),
+            name: value["name"].text_join(),
+            ty: PinType::from(&value["type"].text_join()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum PinType {
+    Input,
+    Output,
+    BiDirectional,
+    PowerIn,
+    PowerOut,
+    Passive,
+    NotConnected,
+    TriState,
+    // Unspecified, TODO:
+    // OpenCollector, TODO:
+    // OpenEmitter TODO:
+}
+
+impl<S: AsRef<str>> From<S> for PinType {
+    fn from(str: S) -> Self {
+        let str = str.as_ref();
+
+        match str.to_lowercase().as_str() {
+            "passive" => PinType::Passive,
+            "input" => PinType::Input,
+            "output" => PinType::Output,
+            "bidi" => PinType::BiDirectional,
+            "power_in" => PinType::PowerIn,
+            "power_out" => PinType::PowerOut,
+            "notconnected" => PinType::NotConnected,
+            "3state" => PinType::TriState,
+            _ => unimplemented!("The pin type {} has not yet been implemented", str),
+        }
+    }
 }
--- src/sexpr.rs
+++ src/sexpr.rs
@@ -5,11 +5,17 @@ use lexpr::Value;
 pub type Text<'t> = Cow<'t, str>;
 
 pub trait IntoText<'a>: lexpr::Index {
-    fn text(&'a self) -> Text<'a>;
+    fn into_text_internal(&'a self, join: bool) -> Text<'a>;
+    fn text(&'a self) -> Text<'a> {
+        self.into_text_internal(false)
+    }
+    fn text_join(&'a self) -> Text<'a> {
+        self.into_text_internal(true)
+    }
 }
 
 impl<'a> IntoText<'a> for Value {
-    fn text(&'a self) -> Text<'a> {
+    fn into_text_internal(&'a self, join: bool) -> Text<'a> {
         match self {
             Value::Nil | Value::Null => Cow::Borrowed(""),
             Value::Bool(b) => Cow::Owned(b.to_string()),
@@ -19,9 +25,25 @@ impl<'a> IntoText<'a> for Value {
             Value::Symbol(s) => Cow::Borrowed(&s),
             Value::Keyword(k) => Cow::Borrowed(&k),
             Value::Bytes(b) => String::from_utf8_lossy(&b),
-            Value::Cons(_) | Value::Vector(_) => {
-                unimplemented!("expected single value, found `{}`", self.to_string())
+            Value::Cons(c) if join => {
+                let (left, right) = c.as_pair();
+
+                if right.is_nil() || right.is_null() {
+                    return left.into_text_internal(join);
+                }
+
+                let mut joined = left.into_text_internal(join).to_string();
+                joined.push_str(&right.into_text_internal(join));
+
+                Cow::Owned(joined)
             }
+            Value::Vector(v) if join => {
+                Cow::Owned(v.iter().map(|v| v.into_text_internal(join)).collect::<String>())
+            }
+            _ => unimplemented!(
+                "expected single value, found `{}`. Use `text_join` if these should be one",
+                self.to_string()
+            ),
         }
     }
 }
